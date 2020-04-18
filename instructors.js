d9b62d098f0066d2392e6c8f6f0434104b9ffe97
Dimas Alves Pereira <dimasdevspro@gmail.com>
2020-04-18 17:14:45 -0300

    create data com fs

--- instructors.js
+++ instructors.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const data = require('./data.json')
 // create
 exports.post = function(req, res){
     // req.query
@@ -10,7 +12,18 @@ exports.post = function(req, res){
          return res.send('Please, fill all fields!')
      }
         }
-    return res.send(req.body)
+req.body.birth = Date.parse(req.body.birth)
+req.body.created_at = Date.now()
+        // []
+data.instructors.push(req.body) // [{...}]
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+            if (err) return res.send("Write file error!")
+
+            return res.redirect("/instructors")
+    } )
+        
+    //return res.send(req.body)
 }
 
 // update
