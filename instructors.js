d9a3f25fbf1f256eb792e7c2f38fc5a935eb2ef3
Dimas Alves Pereira <dimasdevspro@gmail.com>
2020-04-19 09:35:04 -0300

    estruturando dados

--- instructors.js
+++ instructors.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 const data = require('./data.json')
-// create
+
 exports.post = function(req, res){
-    // req.query
-    // req.body
 
     const keys = Object.keys(req.body)
 
@@ -12,10 +10,22 @@ exports.post = function(req, res){
          return res.send('Please, fill all fields!')
      }
         }
-req.body.birth = Date.parse(req.body.birth)
-req.body.created_at = Date.now()
-        // []
-data.instructors.push(req.body) // [{...}]
+
+        let {avatar_url, birth, created_at, id, name, services, gender} = req.body
+
+birth = Date.parse(birth)
+created_at = Date.now()
+id = Number(data.instructors.length + 1)
+
+data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at
+}) 
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
             if (err) return res.send("Write file error!")
@@ -23,9 +33,4 @@ data.instructors.push(req.body) // [{...}]
             return res.redirect("/instructors")
     } )
         
-    //return res.send(req.body)
 }
-
-// update
-
-// delete
